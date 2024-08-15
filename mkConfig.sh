f1671521116511e85b5002f709877b8995899423
comedicomedy <88862122+comedicomedy@users.noreply.github.com>
2024-08-14 23:14:20 -0400

    Add files via upload

---
+++ mkConfig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pacman -Syu
+pacman -S sddm i3-wm nvim xrandr arandr polkit-kde-agent alacritty firefox discord spotify-installer pipewire wireplumber nitrogen rofi thunar flameshot xorg-xrdb sudo git nvida-open ttf-cascadia-code ttf-cascadia-code-nerd zsh neofetch keepass
+
+systemctl enable sddm.service
+systemctl enable NetworkManager.service
+
+useradd -m -G sudo comedic
+passwd comedic
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
